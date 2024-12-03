94c6ca060b19af75b0c1351ff111b0052882b228
jyotiprokash <jyotiprokash2000@gmail.com>
2024-12-03 21:47:52 +0600

    first commit,added file setup & install

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayouts = () => {
+    return (
+        <div>
+                <h1>main layouts</h1>
+        </div>
+    );
+};
+
+export default MainLayouts;
